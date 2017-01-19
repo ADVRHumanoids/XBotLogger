f8c9c5617814e3cbcc399d0cf63e100496fd7b64
Arturo Laurenzi <arturo.laurenzi@iit.it>
2017-01-19 11:14:24 +0100

    Now working

--- Logger.hpp
+++ Logger.hpp
@@ -126,11 +126,11 @@ public:
     friend void operator<< ( std::ostream& ss, LoggerEndl& log );
 
 protected:
-    
+
     virtual void info_internal( const std::string& s) = 0;
     virtual void warning_internal( const std::string& s) = 0;
     virtual void error_internal( const std::string& s) = 0;
-        
+
     std::string _logger_name;
     std::string _log_filename;
     int _log_file_size;
@@ -166,7 +166,7 @@ private:
         // empty the stringstream
         _ss.str(std::string());
     }
-    
+
     std::stringstream _ss;
     std::string _header;
 
@@ -176,7 +176,7 @@ private:
 };
 
 class SSFileLogger : public SSLogger {
-    
+
 public:
      /**
      * @brief SSLogger constructor
@@ -187,8 +187,8 @@ public:
                  const std::string& log_filename,
                  int log_file_size,
                  int log_max_files ) :
-        SSLogger(logger_name, 
-                 log_filename, 
+        SSLogger(logger_name,
+                 log_filename,
                  log_file_size,
                  log_max_files)
     {
@@ -199,7 +199,7 @@ public:
 };
 
 class SPDFileLogger : public SSFileLogger {
-    
+
 public:
      /**
      * @brief SSLogger constructor
@@ -210,12 +210,12 @@ public:
                      const std::string& log_filename,
                      int log_file_size,
                      int log_max_files ) :
-        SSFileLogger( logger_name, 
-                      log_filename, 
+        SSFileLogger( logger_name,
+                      log_filename,
                       log_file_size,
                       log_max_files)
     {
-        
+
         // by default use the async mode to be RT safe
         size_t q_size = ASYNC_QUEUE_SIZE_BIT;
         spdlog::set_async_mode ( q_size );
@@ -223,30 +223,30 @@ public:
         // rotating file logger
         _rotating_logger = spdlog::rotating_logger_mt ( logger_name, log_filename, log_file_size, log_max_files );
     }
-    
+
 protected:
-    
+
     virtual void info_internal( const std::string& s) {
         _rotating_logger->info ( s );
     }
-    
+
     virtual void warning_internal( const std::string& s) {
         _rotating_logger->warn ( s );
     }
-    
+
     virtual void error_internal( const std::string& s) {
         _rotating_logger->error ( s );
     }
-    
+
 
 private:
-    
+
     std::shared_ptr<spdlog::logger> _rotating_logger;
 
 };
 
 class SSConsoleLogger : public SSLogger {
-    
+
 public:
      /**
      * @brief SSLogger constructor
@@ -254,8 +254,8 @@ public:
      *
      */
      SSConsoleLogger ( const std::string& logger_name ) :
-        SSLogger(logger_name, 
-                 "/tmp/XBotConsoleLog", 
+        SSLogger(logger_name,
+                 "/tmp/XBotConsoleLog",
                  1048576 * 5,
                  5)
     {
@@ -265,7 +265,7 @@ public:
 };
 
 class SPDConsoleLogger : public SSConsoleLogger {
-    
+
 public:
      /**
      * @brief SSLogger constructor
@@ -280,27 +280,27 @@ public:
         // rotating file logger
         _rotating_logger = spdlog::rotating_logger_mt ( logger_name, _log_filename, _log_file_size, _log_max_files );
     }
-    
+
 protected:
-    
+
     virtual void info_internal( const std::string& s) {
         _console->info ( s );
         _rotating_logger->info ( s );
     }
-    
+
     virtual void warning_internal( const std::string& s) {
         _console->warn ( s );
         _rotating_logger->warn ( s );
     }
-    
+
     virtual void error_internal( const std::string& s) {
         _console->error ( s );
         _rotating_logger->error ( s );
     }
-    
+
 
 private:
-    
+
     std::shared_ptr<spdlog::logger> _console;
     std::shared_ptr<spdlog::logger> _rotating_logger;
 
@@ -310,8 +310,8 @@ private:
 class Logger {
 
 public:
-    
-    
+
+
     /**
     * @brief Info log
     *
@@ -337,8 +337,7 @@ public:
      *
      * @return XBot::SSLogger&
      */
-    virtual SSLogger& error() {const std::string& logger_name,
-                const std::string& log_filename )
+    virtual SSLogger& error() {
         _ss_logger->setSeverityLevel ( XBot::LoggerLevel::ERROR );
         return *_ss_logger;
     }
@@ -351,9 +350,9 @@ public:
     LoggerEndl& endl() {
         return _ss_logger->endl();
     }
-    
+
 protected:
-    
+
      std::shared_ptr<SSLogger> _ss_logger;
 
 };
@@ -361,45 +360,47 @@ protected:
 class FileLogger : public Logger {
 
 public:
-    
+
     typedef std::shared_ptr<FileLogger> Ptr;
-    
+
     static Ptr getLogger( const std::string& logger_name,
                  const std::string& log_filename,
                  int log_file_size = 1048576 * 5,
                  int log_max_files = 5 ){
-     
+
         if(_instance_map.count(logger_name)) return _instance_map.at(logger_name);
         else{
             Ptr ptr(new FileLogger(logger_name, log_filename, log_file_size, log_max_files));
             _instance_map[logger_name] = ptr;
             return ptr;
         }
-        
+
     }
 
-    
-    
+
+
 private:
-    
+
     FileLogger ( const std::string& logger_name,
                  const std::string& log_filename,
                  int log_file_size = 1048576 * 5,
-                 int log_max_files = 5 ) 
+                 int log_max_files = 5 )
     {
         _ss_logger = std::make_shared<SPDFileLogger>(logger_name, log_filename, log_file_size, log_max_files);
     }
-    
+
     static std::map<std::string, Ptr> _instance_map;
 
-}; 
+};
+
+std::map<std::string, FileLogger::Ptr> FileLogger::_instance_map;
 
 class ConsoleLogger : public Logger {
 
 public:
 
     typedef std::shared_ptr<ConsoleLogger> Ptr;
-    
+
     static Ptr getLogger(const std::string& logger_name = "console_logger"){
         if(_instance) return _instance;
         else{
@@ -407,9 +408,9 @@ public:
             return _instance;
         }
     }
-    
+
 private:
-    
+
     /**
      * @brief XBot Logger constructor: you can specify the logger name, the path for the log,
      * the file_size and the maximum number of file to use for the log.
@@ -417,37 +418,28 @@ private:
      * By default it creates the log using file /tmp/XBotLog with 5Mb and a maximum of 5 files.
      *
      */
-    
+
     ConsoleLogger ( const std::string& logger_name)
     {
         _ss_logger = std::make_shared<SPDConsoleLogger>(logger_name);
     }
-    
+
     static Ptr _instance;
-    
+
 
 };
+
+ConsoleLogger::Ptr ConsoleLogger::_instance;
 
 
 class MatLogger {
 
 public:
-    
+
     typedef std::shared_ptr<MatLogger> Ptr;
-    
+
     static Ptr getLogger( const std::string& logger_name,
-                 const std::string& log_filename){
-     
-        if(_instance_map.count(logger_name)) return _instance_map.at(logger_name);
-        else{
-            Ptr ptr(new FileLogger(logger_name, log_filename));
-            _instance_map[logger_name] = ptr;
-            return ptr;
-        }
-        
-    }
-    
-    
+                 const std::string& log_filename);
 
     template < typename Derived >
     void add( const std::string& var_name, const Eigen::MatrixBase<Derived>& var) {
@@ -462,63 +454,61 @@ public:
             // create the var in the _var_count_map
             _var_count_map[var_name] = 1;
             current_cout = 1;
-            
+
             // set the var _var_dim_map
             _var_dim_map[var_name] = std::make_pair<int,int>(var.rows(), var.cols());
-            
+
             // name of the variable
             ss << var_name;
-            
+
             //check if matrix or vector and allocated it
             if(var.cols() == 1) {
                 // NOTE only column vecotrs
-                ss << "(" << var.rows() << "," << M_PREALLOCATION_SIZE << ") = 0;" << std::endl; 
+                ss << "(" << var.rows() << "," << M_PREALLOCATION_SIZE << ") = 0;" << std::endl;
             }
             else {
                 // NOTE matrix is a cube
-                ss << "(" << var.rows() << "," << var.cols() << "," << M_PREALLOCATION_SIZE << ") = 0;" << std::endl; 
+                ss << "(" << var.rows() << "," << var.cols() << "," << M_PREALLOCATION_SIZE << ") = 0;" << std::endl;
             }
- 
+
         }
-        
+
         if(_var_dim_map.at(var_name).first != var.rows() ||
-           _var_dim_map.at(var_name).second != var.cols() )static std::map<std::string, Ptr> _instance_map; {
+           _var_dim_map.at(var_name).second != var.cols() ){
             // NOTE do it on the logger
             std::cerr << "Error in " << __func__ << " : provided Matrix/Vector has mismatching dimensions" << std::endl;
             return;
         }
-        
+
         //check if matrix or vector and fill it
         if(var.cols() == 1) {
             // NOTE only column vectors
-            ss << var_name << "(:," << current_cout << ") = " << var.format(_mat_fmt) << ";" << std::endl; 
+            ss << var_name << "(:," << current_cout << ") = " << var.format(_mat_fmt) << ";" << std::endl;
         }
         else {
             // NOTE matrix is a cube
-            ss << var_name << "(:,:," << current_cout << ") = " << var.format(_mat_fmt) << ";" << std::endl; 
+            ss << var_name << "(:,:," << current_cout << ") = " << var.format(_mat_fmt) << ";" << std::endl;
         }
-        
+
         add_internal(ss.str() );
 
     }
-    
+
     virtual void add( const std::string& var_name, float var) {
         _scalar_var[0] = var;
         add(var_name, _scalar_var);
     }
-    
+
     virtual ~MatLogger() {
         // TBD how to do it?
 //         std::cout << "~~~~~~~MatLogger" << std::endl;
 //         close();
     }
-        
+
 protected:
-    
+
     virtual void add_internal( const std::string& var_string) = 0;
-    
-private:
-    
+
     MatLogger ( const std::string& logger_name,
                 const std::string& log_filename ) :
         _logger_name(logger_name),
@@ -526,44 +516,45 @@ private:
         _scalar_var(1),
         _mat_fmt(Eigen::FullPrecision, 0, ", ", ";\n", "", "", "[", "]")
     {
-        
+
     }
-    
+private:
+
     void close() {
         for(auto m_var_count : _var_count_map) {
             std::stringstream ss;
             if(_var_dim_map.at(m_var_count.first).first == 1) {
                 // NOTE only column vecotrs
-                ss << m_var_count.first << "=" << m_var_count.first << "(:,1:" << m_var_count.second << ");" << std::endl; 
+                ss << m_var_count.first << "=" << m_var_count.first << "(:,1:" << m_var_count.second << ");" << std::endl;
             }
             else {
                 // NOTE matrix is a cube
-                ss << m_var_count.first << "=" << m_var_count.first << "(:,:,1:" << m_var_count.second << ");" << std::endl; 
+                ss << m_var_count.first << "=" << m_var_count.first << "(:,:,1:" << m_var_count.second << ");" << std::endl;
             }
-            
+
             add_internal(ss.str());
         }
     }
-        
+
     Eigen::IOFormat _mat_fmt;
-    
+
     std::string _logger_name;
     std::string _log_filename;
-    
+
     std::string _var_string;
-    
+
     Eigen::VectorXd _scalar_var;
-    
+
     std::unordered_map<std::string, int> _var_count_map;
     std::unordered_map<std::string, std::pair<int,int>> _var_dim_map;
-    
+
     static std::map<std::string, Ptr> _instance_map;
-        
-                
+
+
 };
 
 class SPDMatLogger : public MatLogger {
-    
+
 public:
      /**
      * @brief SSLogger constructor
@@ -574,30 +565,30 @@ public:
                       const std::string& log_filename ) :
         MatLogger(logger_name, log_filename)
     {
-        
+
         // by default use the async mode to be RT safe
         size_t q_size = ASYNC_QUEUE_SIZE_BIT;
         spdlog::set_async_mode ( q_size );
-        
+
         // set m pattern
         spdlog::set_pattern("%v");
 
         // rotating file logger
         _simple_logger = spdlog::basic_logger_mt ( logger_name, log_filename );
     }
-    
+
     virtual ~SPDMatLogger(){
 //         std::cout << "~~~~~~~SPDMatLogger" << std::endl;
     }
-    
+
 protected:
-    
-    void add_internal( const std::string& var_string) { 
+
+    void add_internal( const std::string& var_string) {
         _simple_logger->info(var_string);
     }
 
 private:
-    
+
     std::shared_ptr<spdlog::logger> _simple_logger;
 
 };
@@ -605,6 +596,20 @@ private:
 // now that SSLogger is completely defined we can implment the print()
 void LoggerEndl::print ( std::ostream& ss ) {
     _ss_logger->print ( ss );
+}
+
+std::map<std::string, MatLogger::Ptr> MatLogger::_instance_map;
+
+MatLogger::Ptr MatLogger::getLogger(const std::string& logger_name, const std::string& log_filename)
+{
+
+    if(_instance_map.count(logger_name)) return _instance_map.at(logger_name);
+    else{
+        Ptr ptr(new SPDMatLogger(logger_name, log_filename));
+        _instance_map[logger_name] = ptr;
+        return ptr;
+    }
+
 }
 
 
