6e2dcb315474ab17f384d484e0fb6ef929c25d34
Arturo Laurenzi <arturo.laurenzi@iit.it>
2017-10-25 15:42:01 +0200

    Removed useless dynamic cast

--- RtLog.hpp
+++ RtLog.hpp
@@ -2,15 +2,15 @@
 #include <boost/iostreams/device/array.hpp>
 #include <iostream>
 
-#define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
+#define RT_LOG_RESET   "\033[0m"
+#define RT_LOG_BLACK   "\033[30m"      /* Black */
+#define RT_LOG_RED     "\033[31m"      /* Red */
+#define RT_LOG_GREEN   "\033[32m"      /* Green */
+#define RT_LOG_YELLOW  "\033[33m"      /* Yellow */
+#define RT_LOG_BLUE    "\033[34m"      /* Blue */
+#define RT_LOG_MAGENTA "\033[35m"      /* Magenta */
+#define RT_LOG_CYAN    "\033[36m"      /* Cyan */
+#define RT_LOG_WHITE   "\033[37m"      /* White */
 
 #ifndef DPRINTF
 
@@ -24,11 +24,17 @@
 
 #endif
 
+#include <pthread.h>
+
 
 namespace XBot { namespace experimental {
     
     class Logger;
     
+    /**
+     * @brief Class handling the flushing of log messages to console.
+     * 
+     */
     class Endl {
         
     public:
@@ -59,23 +65,25 @@ namespace XBot { namespace experimental {
     
     std::ostream& color_green(std::ostream& os)
     {
-        return os << GREEN;
+        return os << RT_LOG_GREEN;
     }
     
     std::ostream& color_red(std::ostream& os)
     {
-        return os << RED;
+        return os << RT_LOG_RED;
     }
     
     std::ostream& color_yellow(std::ostream& os)
     {
-        return os << YELLOW;
+        return os << RT_LOG_YELLOW;
     }
     
     std::ostream& color_reset(std::ostream& os)
     {
-        return os << RESET;
+        return os << RT_LOG_RESET;
     }
+    
+    
     
     class Logger {
         
@@ -143,9 +151,7 @@ namespace XBot { namespace experimental {
     
     void operator<< ( std::ostream& os, Endl& endl )
     {
-        auto& l_ios = dynamic_cast<Logger::IoStream&>(os);
         endl.print();
-        l_ios.close();
     }
     
     void Endl::print()
@@ -157,6 +163,7 @@ namespace XBot { namespace experimental {
     {
         _sink << color_reset;
         DPRINTF("%s\n", _buffer);
+        _sink.close();
     }
 
     
